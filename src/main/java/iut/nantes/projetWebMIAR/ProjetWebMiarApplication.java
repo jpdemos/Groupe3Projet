5169e83b3187e72d1a2e77957c7dd11844bd39ae
'Elymne' <'sacha.djurdjevic@gmail.com'>
2018-12-03 10:28:29 +0100

    Reformatage code

---
+++ src/main/java/iut/nantes/projetWebMIAR/ProjetWebMiarApplication.java
@@ -0,0 +1,12 @@
+package iut.nantes.projetWebMIAR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetWebMiarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetWebMiarApplication.class, args);
+	}
+}
---
+++ src/test/java/iut/nantes/projetWebMIAR/ProjetWebMiarApplicationTests.java
@@ -0,0 +1,16 @@
+package iut.nantes.projetWebMIAR;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetWebMiarApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
